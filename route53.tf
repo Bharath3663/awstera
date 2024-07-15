d76348ab2c30e9705bae5fcd8f7f2052fd30257d
bharath vallabaneni <154310464+Bharath3663@users.noreply.github.com>
2024-07-15 18:02:46 +0530

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -1,12 +1,12 @@
-resource "aws_route53_zone" "primary" {
-  name = var.domain_name
-}
+#resource "aws_route53_zone" "primary" {
+ # name = var.domain_name
+#}
 
 # Define an A record
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = var.record_name
-  type    = "A"
-  ttl     = 300  # Time to live in seconds
-  records = [var.record_ip]
-}
+#resource "aws_route53_record" "www" {
+  #zone_id = aws_route53_zone.primary.zone_id
+  #name    = var.record_name
+  #type    = "A"
+  #ttl     = 300  # Time to live in seconds
+ # records = [var.record_ip]
+#}
